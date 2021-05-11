e547ec56984176477099b051a56f6e5b793f508f
mgpijuan <mgpijuan@gmail.com>
2021-05-11 18:26:57 +0200

    commits

---
+++ sprint1/index.js
@@ -0,0 +1,10 @@
+let nom = "Luke";
+let edat = 1;
+
+edat = edat + 1;
+
+console.log('El meu gos ' + nom + 'te casi ' +  edat)
+
+const buildDate = "October, 25, 2010";
+buildDate = "October, 26, 2011";
+console.log(buildDate);
---
+++ sprint1/variables.js
@@ -0,0 +1,11 @@
+let firstName = "Mònica" ;
+firstName = "Jordi";
+
+let lastName = "González";
+
+let fullName = lastName + ", " + firstName;
+let age = 32;
+let isEmployes = true;
+isEmployes =false;
+
+console.log(fullName);
